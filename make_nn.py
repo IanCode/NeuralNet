4e78e004ff82e1c3ad85aaf9e8f1077e7bc46a8b
Jordan Richards <iwwhite@pugetsound.edu>
2018-05-01 16:45:11 -0700

    neuralnet Test

--- make_nn.py
+++ make_nn.py
@@ -41,85 +41,116 @@ import theano
 from keras.layers import Conv2D, MaxPooling2D
 from keras.models import Sequential
 from keras.preprocessing.image import ImageDataGenerator
-from keras.layers import Flatten, Dense, Activation, Dropout
+from keras.layers import Flatten, Dense, Activation, Dropout, ZeroPadding2D
 from keras import backend
 
 def main():
     print(numpy.__version__)
     print(theano.__version__)
 
-    # dimensions of our images.
-    img_width, img_height = 100, 100
+    width, height = 100, 100
 
     #take training directory as a command line argument
     train_data_dir = sys.argv[1]
-    validation_data_dir = 'validation'
-    nb_train_samples = 12500
-    nb_validation_samples = 5000
+    nb_train_samples = 7000
     epochs = 50
     batch_size = 16
 
-    if backend.image_data_format() == 'channels_first':
+    '''if backend.image_data_format() == 'channels_first':
         input_shape = (3, img_width, img_height)
     else:
-        input_shape = (img_width, img_height, 3)
+        input_shape = (img_width, img_height, 3)'''
+#########################################################
+'''    model = Sequential()
+    model.add(ZeroPadding2D((1,1),input_shape=(3,224,224)))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
 
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(Flatten())
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1000, activation='softmax'))'''
+########################################################
     model = Sequential()
-    model.add(Conv2D(32, (3, 3), input_shape=input_shape))
-    model.add(Activation('elu'))
+    model.add(Conv2D(32, (3, 3), input_shape=(width, height, 3)))
+    model.add(Activation('relu'))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(32, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(32, (3, 3)))
-    model.add(Activation('elu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    #model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(64, (3, 3)))
-    model.add(Activation('elu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(256, (1, 1), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(128, (3, 3)))
-    model.add(Activation('elu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(256, (3, 3)))
-    model.add(Activation('elu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+
 
     model.add(Flatten())
 
-    model.add(Dense(256, activation='elu'))
-
+    model.add(Dense(512, activation='relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(1, activation='sigmoid'))
-    #model.add(Dropout(0.5))
-    #model.add(Dense(1, activation='softmax'))
+    model.add(Dense(1, activation='softmax'))
 
     model.compile(loss='binary_crossentropy',
-                  optimizer='rmsprop',
+                  optimizer='adam',
                   metrics=['accuracy'])
 
-    # this is the augmentation configuration we will use for training
-    train_datagen = ImageDataGenerator(
-        rescale=1. / 255,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True)
+    trainingData = ImageDataGenerator(rescale=1./100)
 
-    '''# this is the augmentation configuration we will use for testing:
-    # only rescaling
-    test_datagen = ImageDataGenerator(rescale=1. / 255)'''
-
-    train_generator = train_datagen.flow_from_directory(
+    train_generator = trainingData.flow_from_directory(
         train_data_dir,
-        target_size=(img_width, img_height),
+        target_size=(width, height),
         batch_size=batch_size,
         class_mode='binary')
-
-    '''validation_generator = test_datagen.flow_from_directory(
-        validation_data_dir,
-        target_size=(img_width, img_height),
-        batch_size=batch_size,
-        class_mode='binary')'''
 
     model.fit_generator(
         train_generator,
